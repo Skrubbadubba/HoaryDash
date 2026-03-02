ae598d5cd064eec7b2b9bee185cea50e38abd3e9
Skrubba <gustafenorberg@gmail.com>
2026-03-02 18:14:00 +0100

    fungerande slider

--- server/main.go
+++ server/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "4567"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4567"
+	}
 
-    fs := http.FileServer(http.Dir("/app/frontend/static"))
-    http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("/app/frontend/static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/api", helloHandler)
 
-    log.Printf("Serving on :%s", port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("Serving on :%s", port)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
 }
